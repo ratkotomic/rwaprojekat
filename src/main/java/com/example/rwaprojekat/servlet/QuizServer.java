078cd68bd36df9710a7a1a062b1d10a493d8c9c7
Omar.Kurtovic <omar.kurtovic@fet.ba>
2023-08-21 21:26:29 +0200

    slowly beginning to play quiz

--- src/main/java/com/example/rwaprojekat/servlet/QuizServer.java
+++ src/main/java/com/example/rwaprojekat/servlet/QuizServer.java
@@ -1,52 +1,65 @@
 package com.example.rwaprojekat.servlet;
 
 import com.example.rwaprojekat.dao.QuizDao;
-import com.example.rwaprojekat.decoders.*;
+import com.example.rwaprojekat.model.Message;
 import com.example.rwaprojekat.model.Question;
 import com.example.rwaprojekat.model.Quiz;
-import jakarta.websocket.EncodeException;
-import jakarta.websocket.OnMessage;
-import jakarta.websocket.OnOpen;
-import jakarta.websocket.Session;
+import jakarta.websocket.*;
 import jakarta.websocket.server.ServerEndpoint;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.concurrent.CopyOnWriteArraySet;
 
 
 @ServerEndpoint(
-        value = "/quiz",
-        decoders = QuestionDecoder.class,
-        encoders = QuestionEncoder.class )
+        value = "/quiz")
 public class QuizServer {
-    private final static Set<Session> sessions =  new CopyOnWriteArraySet<Session>();
+    private final static ArrayList<Session> sessions = new ArrayList<>();
     private Quiz quiz;
-    private int currQuestion = 0;
     @OnOpen
-    public void onOpen(
-            Session session) throws IOException {
+    public void onOpen(Session session) {
         sessions.add(session);
-        QuizDao quizDao = new QuizDao();
-        quiz = quizDao.findQuizById("402881838a143fbf018a145a4113003e");
+        try{
+            session.getBasicRemote().sendText("402881838a143fbf018a145a4113003e");
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+
+        handleTextMessage("users" + sessions.size());
     }
 
+
+
     @OnMessage
-    public Question handleNextQuestion(String message)
-    {
-        List<Question> questions = quiz.getQuestions();
-        Question question = questions.get(currQuestion);
+    public String handleTextMessage(String message) {
+
         sessions.forEach(s -> {
             try {
-                s.getBasicRemote().sendObject(question);
-            } catch (IOException | EncodeException e) {
+                s.getBasicRemote().sendText(message);
+            } catch (Exception e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         });
-        currQuestion++;
-        return question;
+        return message;
+    }
 
+    public Quiz sendQuizToClient(Quiz quiz, Session session)
+    {
+        try {
+            session.getBasicRemote().sendObject(quiz);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return quiz;
+    }
+
+    @OnClose
+    public void onClose(Session session) throws IOException {
+        sessions.remove(session);
+        System.out.println("Session removed: " + session.getId());
     }
 }
